1e36fd32401f918dda8c4267b100c0c5ebc4ecce
DanielSMC20 <140360236+DanielSMC20@users.noreply.github.com>
2024-05-03 10:58:51 +0000

    Termino

--- scrip.js
+++ scrip.js
@@ -3,31 +3,28 @@ let searchBtn = document.getElementById("search-btn");
 let cityRef = document.getElementById("city");
 
 let getWeather = () => {
-    let cityValue = cityRef.value;
-    if(cityValue.length ==0){
-        result.innerHTML = `<h3 class="msg"> Please enter a city name</h3>`;
-    }
-    else{
-        let url =
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
-        cityRef.value="";
-        fetch(url)
-        .then((resp) =>resp.json())
-    .then(data => {
+  let cityValue = cityRef.value;
+  if (cityValue.length == 0) {
+    result.innerHTML = `<h3 class="msg"> Please enter a city name</h3>`;
+  } else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+    cityRef.value = "";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
         console.log(data);
         console.log(data.weather[0].icon);
         console.log(data.weather[0].main);
         console.log(data.weather[0].description);
         console.log(data.name);
-        console.log(data.main)
+        console.log(data.main);
         console.log(data.main.temp_min);
         console.log(data.main.temp_max);
         result.innerHTML = `
         <h2>${data.name}</h2>
         <h4 class="weather">${data.weather[0].main}
         </h4>
-        <h4 class="description">${data.weather[0].
-            description}</h4>
+        <h4 class="description">${data.weather[0].description}</h4>
         <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
         <h1>${data.main.temp} &#176;</h1>
         <div class="temp-container">
@@ -41,13 +38,12 @@ let getWeather = () => {
             </div>
         </div>
         `;
-        
-    })
+      })
 
-    .catch(()=>{
-        result.innerHTML =`<h3 class="msg">City not fount</h3>`;
-    })
-    }
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">City not fount</h3>`;
+      });
+  }
 };
-searchBtn.addEventListener("click",getWeather); 
-window.addEventListener("load",getWeather);
+searchBtn.addEventListener("click", getWeather);
+window.addEventListener("load", getWeather);
