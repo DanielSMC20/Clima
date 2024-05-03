4b8e9952901eb7fdee5f36d14c21f7585c8a7cad
DanielSMC20 <140360236+DanielSMC20@users.noreply.github.com>
2024-05-03 22:04:27 +0000

    final

--- scrip.js
+++ scrip.js
@@ -7,7 +7,8 @@ let getWeather = () => {
   if (cityValue.length == 0) {
     result.innerHTML = `<h3 class="msg"> Please enter a city name</h3>`;
   } else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric
+`;
     cityRef.value = "";
     fetch(url)
       .then((resp) => resp.json())
@@ -24,7 +25,7 @@ let getWeather = () => {
         <h2>${data.name}</h2>
         <h4 class="weather">${data.weather[0].main}
         </h4>
-        <h4 class="description">${data.weather[0].description}</h4>
+        <h4 class="desc">${data.weather[0].description}</h4>
         <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
         <h1>${data.main.temp} &#176;</h1>
         <div class="temp-container">
